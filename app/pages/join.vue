ce90fc170e03333ee40c40369674ca5a0175e54e
kevinlee-06 <cgshyuankai@proton.me>
2025-02-23 22:08:24 +0800

    feat: enhance join page with detailed content and improved layout

--- app/pages/join.vue
+++ app/pages/join.vue
@@ -1,4 +1,97 @@
 <template>
-  <h1>Join us!</h1>
-  <div />
+  <div class="npc-paragraph">
+    <h1>加入社團</h1>
+    <div class="npc-box">
+      <h2>社員</h2>
+      <div class="project-content">
+        <img
+          alt=""
+          class="project-image"
+          src="https://i.imgur.com/rkobsqy.webp"
+        >
+        <div class="project-description">
+          <h3>🌟 加入我們的社團吧！🌟</h3>
+          <p>🎉 成為 <strong>我們的一員</strong>，您將獲得：</p>
+          <ul>
+            <li>獨家社服一件，展現您的團隊精神！</li>
+            <li>精彩的社課，提升您的技能與知識！</li>
+          </ul>
+          <br>
+          <p>💰 社費僅需 500 元，讓您享受所有會員專屬福利！</p>
+          <p>📢 建議加入我們的 Discord 群組，隨時掌握最新消息與活動資訊，與其他會員互動交流！</p>
+          <p>快來加入我們的大家庭，一起創造美好的回憶！💖</p>
+          <p>有興趣的朋友請點擊下方 Discord 連結，或透過 Email 聯絡我們！</p>
+        </div>
+      </div>
+      <div class="download-badge">
+        <a href="https://to.ntut.club/discord"><button>
+          <strong>
+            加入 Discord
+          </strong>
+        </button></a>
+        <a href="mailto:contact@ntut.club"><button>
+          Email
+        </button></a>
+      </div>
+    </div>
+
+    <div class="npc-box">
+      <h2>線上成員</h2>
+      <div class="project-content">
+        <img
+          alt=""
+          class="project-image"
+          src="https://www.torproject.org/static/images/download/svg/stay-safe.svg?h=f11bdbfa"
+        >
+        <div class="project-description">
+          <h3>
+            還在猶豫嗎？先加入 Discord 群組吧！
+          </h3>
+          <p>加入社群，您將獲得：</p>
+          <ul>
+            <li>公開頻道存取權限！</li>
+            <li>與電神交流的機會！</li>
+            <li>了解更多社團活動！</li>
+          </ul>
+          <br>
+          <a href="https://to.ntut.club/discord"><button>
+            <strong>
+              加入 Discord
+            </strong>
+          </button></a>
+        </div>
+      </div>
+      <div class="download-badge">
+        <p>💰 好吃的古早味社課，每次只要 50 元，歡迎來看看！</p>
+      </div>
+    </div>
+
+    <div class="npc-box">
+      <h2>僅購買社服</h2>
+      <div class="project-content">
+        <img
+          alt=""
+          class="project-image"
+          src="assets/t-shirt.jpg"
+        >
+        <div class="project-description">
+          <h3>
+            還在猶豫嗎？不如先買件社服吧！
+          </h3>
+          <p>購買社服，您將獲得社員的所有權利</p>
+          <br>
+          <a href="https://to.ntut.club/discord"><button>
+            <strong>
+              聯絡銷售團隊
+            </strong>
+          </button></a>
+          <br>
+          <br>
+          <h5>網友留言：已購買，小孩愛穿</h5>
+        </div>
+        <p>💰 好穿的古早味社服，每件只要 500 元，歡迎穿看看！</p>
+      </div>
+    </div>
+    <div class="download-badge" />
+  </div>
 </template>
