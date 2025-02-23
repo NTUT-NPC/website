c841cd6d4dcb42ede2983aa37c353dee6dcb15eb
kevinlee-06 <cgshyuankai@proton.me>
2025-02-23 20:49:55 +0800

    feat: update projects page with TAT - 北科生活 details and download links

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="projects">
-    <h1>Projects</h1>
+  <div class="npc-paragraph">
+    <h1>專案</h1>
+    <div class="npc-box">
+      <h2>TAT - 北科生活</h2>
+      <div class="project-content">
+        <img
+          alt="北科生活"
+          class="project-image"
+          src="https://i.imgur.com/pMSmPA8.webp"
+        >
+        <div class="project-description">
+          <p>方便，簡潔，快速，強大，豐富您的北科生活！</p>
+          <p>迅速查詢北科學生課表與行事曆，查看北科 i 學園公告，下載北科 i 學園檔案</p>
+          <p>查閱北科學生成績與學分資訊，並主動推播重要消息</p>
+          <p>支援中文、英文顯示，老師亦可使用</p>
+          <p>即刻安裝，北科生活無煩惱～</p>
+          <p>歡迎您提供寶貴的意見、錯誤回報、創意點子給我們。你的想法，我們實現！</p>
+        </div>
+      </div>
+      <div class="download-badge">
+        <a href="https://to.ntut.club/tat/ios">
+          <img
+            alt="Download on the App Store"
+            src="assets/Download_on_the_App_Store_Badge_CNTC_RGB_blk_100217.svg"
+          >
+        </a>
+        <a href="https://to.ntut.club/tat/android">
+          <img
+            alt="Get it on Google Play"
+            src="assets/GetItOnGooglePlay_Badge_Web_color_Chinese-TW.png"
+          >
+        </a>
+      </div>
+      <!-- <a href="https://to.ntut.club/tat">顯示 QR 碼</a>
+      <img src="https://files.ntut.club/projects/tat/qrcode.png" style="display: none;"> -->
+    </div>
   </div>
 </template>
