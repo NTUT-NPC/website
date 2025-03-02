39b135e0221cef46010d2a86742d08402dd7db9a
kevinlee-06 <cgshyuankai@proton.me>
2025-03-03 00:25:50 +0800

    fix: improve current path normalization in error handling

--- app/error.vue
+++ app/error.vue
@@ -2,11 +2,15 @@
 const router = useRouter()
 const { currentRoute } = router
 let currentPath = currentRoute.value.fullPath
-if (currentPath[0] === '/') {
-  currentPath = currentPath.slice(1)
-}
-if (currentPath[currentPath.length - 1] === '/') {
-  currentPath = currentPath.slice(0, -1)
+if (currentPath === '/') {
+  currentPath = ''
+} else {
+  if (currentPath[0] === '/') {
+    currentPath = currentPath.slice(1)
+  }
+  if (currentPath[currentPath.length - 1] === '/') {
+    currentPath = currentPath.slice(0, -1)
+  }
 }
 const targetUrl = `https://to.ntut.club/api/try?slug=${currentPath}&fallback=${location.protocol}//${location.host}/404`
 
